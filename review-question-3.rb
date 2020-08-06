d76600eaf175e33243a8943d2bb33fdc840682aa
wlcreate <waverley.nyc@gmail.com>
2020-08-06 12:48:07 -0400

    DONE

--- review-question-3.rb
+++ review-question-3.rb
@@ -1,24 +1,50 @@
+require 'pry'
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
 
 class User
     attr_accessor :name, :comment, :photo
-    
+    @@all = []
+
     def initialize(name)
         @name = name
+        @@all << self
     end
+
+    def self.all
+        @@all
+    end
+
+    def photos
+        Photo.all
+    end
+
+    def photo
+        Photo.new
+    end
+    # binding.pry
 end
 
 class Photo
-    attr_accessor :user, :photo
+    attr_accessor :user, :photo, :comment
     @@all = []
 
     def initialize
         @@all << self
     end
 
-    def new
+    def self.all
+        @@all
     end
+
+    def comments
+        Comment.all
+    end
+
+    def make_comment(string)
+        Comment.new(string)
+    end
+
 end
 
 class Comment
@@ -29,14 +55,21 @@ class Comment
         @comment = comment
         @@all << self
     end
+
+    def self.all
+        @@all
+    end
 end
 
+
+binding.pry
 sandwich_photo = Photo.new
 sophie = User.new("Sophie")
 sandwich_photo.user = sophie
 sandwich_photo.user.name
 # => "Sophie"
 sophie.photos
+
 # => [#<Photo:0x00007fae2880b370>]
 
 
